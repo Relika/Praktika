2f73bb5672307d8854212f846dce2b17ba65896e
Relika <User@WINDEV1808EVAL>
2018-10-08 07:05:25 -0700

    Tests? CreateTestXML, AddXmlElement, DoesXmlElementExists

--- MicroServiceInstaller3/MicroServiceInstaller3/Tests.cs
+++ MicroServiceInstaller3/MicroServiceInstaller3/Tests.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace MicroServiceInstaller3
@@ -21,37 +22,43 @@ namespace MicroServiceInstaller3
             Assert.IsTrue(c == 9, "Problem");
         }
 
-        [TestMethod]
-        public void CreateTestXML()
+        
+        public string CreateTestXML()
         {
             //olemasolev fail tuleb 'ra kustutada ja siis luua uus
             string temporaryFolderPath = System.IO.Path.GetTempPath();
 
-            string zipPath = System.IO.Path.Combine(temporaryFolderPath, "TestFiles");
-            FShandler.CreateDirectory(zipPath);
+            string testXmlDirectory = System.IO.Path.Combine(temporaryFolderPath, "TestFiles");
+            FShandler.CreateDirectory(testXmlDirectory);
    
-
-            string path = System.IO.Path.Combine(zipPath, "test.exe.config");
-            if (!File.Exists(path))
+            string testXmlPath = System.IO.Path.Combine(testXmlDirectory, "test.exe.config");
+            if (!File.Exists(testXmlPath))
             {
-                // Create a file to write to.
-                using (StreamWriter sw = File.CreateText(path))
-                {
-                    sw.WriteLine(string "<? xml version = "1.0" encoding = "utf-8" ?>
-< configuration >");
-                }
+                XmlWriter xmlWriter = XmlWriter.Create(testXmlPath);
+
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("configuration");
+
+                xmlWriter.WriteStartElement("appSettings");
+
+                xmlWriter.WriteStartElement("add");
+              //  xmlWriter.WriteAttributeString("key", "Wisesite");
+                xmlWriter.WriteAttributeString("value", "SE_30");
+
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
             }
             else
             {
-                File.Delete(path);
+                File.Delete(testXmlPath);
             }
+            return testXmlPath;
         }
 
-
-        [TestMethod]
-        public void XmlSaveTest()
+        public XElement AddXmlElement(string path)
         {
-            string path = @"C:\Downloaded_zip_files\6cb19ed4-b3d2-420b-8e97-6565eb0df8a7\Enics.WiseToSapIntegration.Shipper.vshost.exe.config";
             var doc = XDocument.Load(path);
             var elements = doc.Descendants("appSettings").Elements();
 
@@ -60,17 +67,66 @@ namespace MicroServiceInstaller3
             XAttribute configKeyAttribute = new XAttribute("key", "TestKey");
 
             xmlAddElement.Add(configKeyAttribute);
-            xmlAddElement.Add(configValueAttribute);
+                xmlAddElement.Add(configValueAttribute);
 
 
-            XElement appSettingsElement = doc.Descendants("appSettings").First(); //.First()
+                XElement appSettingsElement = doc.Descendants("appSettings").First(); //.First()
 
             appSettingsElement.Add(xmlAddElement);
-
-
             doc.Save(path);
 
+            return xmlAddElement;
+        }
+
+        public bool DoesXmlElementExist(XElement xmlElement, string path)
+        {
+            var doc = XDocument.Load(path);
+            var elements = doc.Descendants("appSettings").Elements();
+
+            foreach (var item in elements)
+            {
+                //string keyAttributeValue;
+                //string valueAttributeValue;
+
+                if (item.Attribute("key") != null)   //item.Attributes("key") != null
+                {
+                    if (item.Attribute("key").Value == xmlElement.Attribute("key").Value)
+                    {
+                        if (item.Attributes("value") != null)
+                        {
+                            if (item.Attribute("value").Value == xmlElement.Attribute("value").Value)
+                            {
+                                return true;
+                            }
+                            continue;
+                        }
+                        continue;
+                    }
+                    continue;
+                }
+                continue;
+                //XAttribute keyAttribute = item.Attribute("key");
+                //keyAttributeValue = keyAttribute.Value;
+                //{
+                //    XAttribute valueAttribute = item.Attribute("value");
+                //    valueAttributeValue = valueAttribute.Value;
+                //}
+
+            }
+            return false;
+        }
+
+        [TestMethod]
+        public void XmlSaveTest()
+        {
+
+            string path = CreateTestXML();
+            XElement xmlAddElement =  AddXmlElement(path);
+            bool elementExists = DoesXmlElementExist(xmlAddElement, path);
+
+
             //kontrollida, kas lisatud key on failis olemas
+
         }
 
 
